0b2a04a12a7c102df5e52c04a10945409dd46073
Albert Fougy <albert@fougy.com>
2019-10-10 11:27:46 -0400

    finished postgres setup

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,9 +1,21 @@
 # services/users/manage.py
 
+
 from flask.cli import FlaskGroup
-from project import app
+
+from project import app, db 
+
 
 cli = FlaskGroup(app)
+
+
+# new
+@cli.command('recreate_db')
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
 
 if __name__ == '__main__':
     cli()
--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,25 +1,53 @@
 # services/users/project/__init__.py
 
 
+import os  
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy  
 
-# instatiate the app
+
+# instantiate the app
 app = Flask(__name__)
 
 api = Api(app)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')  
+app.config.from_object(app_settings)      
+
+# instantiate the db
+db = SQLAlchemy(app)  
+
+
+# model
+class User(db.Model):  
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 
 class UsersPing(Resource):
     def get(self):
         return {
-            'status':'success',
-            'message': 'pong!',
-            'name': 'Albert'
-        }
+        'status': 'success',
+        'message': 'pong!'
+    }
 
+
+
+# import sys
+# print(app.config, file=sys.stderr)
+
+# Want to test, to ensure the proper config was loaded?
+
+# Add a print statement to __init__.py, right before the route handler, 
+# to view the app config to ensure that it is working:
 
 api.add_resource(UsersPing, '/users/ping')
