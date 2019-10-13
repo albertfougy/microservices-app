c3374e2138bbb604dd3df514f80749ba8188987e
Albert Fougy <albert@fougy.com>
2019-10-13 09:38:58 -0400

    salicious commit

--- services/users/project/tests/test_config.py
+++ services/users/project/tests/test_config.py
@@ -10,3 +10,11 @@ class TestDevelopmentConfig(TestCase):
     def create_app(self):
         app.config.from_object('project.config.DevelopmentConfig')
         return app
+
+    def test_app_is_development(self):
+        self.assertTrue(app.config['SECRET_KEY'] == 'my_precious' )
+        self.assertFalse(app.config is None)
+        self.assertTrue(
+            app.config['SQLALCHEMY_DATABASE_URI'] == 
+            os.get.environ('DATABASE_URL')
+            )
