c887b056e6010e1304204113e6cae35da38df4a0
Albert Fougy <albert@fougy.com>
2019-10-15 12:41:54 -0400

    added add_test_user

--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -15,6 +15,22 @@ class TestUserService(BaseTestCase):
         self.assertIn('pong!', data['message'])
         self.assertIn('success', data['status'])
 
+    def test_add_user(self):
+        """Ensure a new user can be added to the database."""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps({
+                    'username': 'albert',
+                    'email': 'albert@fougy.com'
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('albert@fougy.com was added!', data['message'])
+            self.assertIn('success', data['status'])
+
 
 if __name__ == '__main__':
     unittest.main()
