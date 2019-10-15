1710233fc6697930c76d2a11e623566e43f19b57
Albert Fougy <albert@fougy.com>
2019-10-15 11:35:59 -0400

    added test setup

--- services/users/project/tests/test_config.py
+++ services/users/project/tests/test_config.py
@@ -15,7 +15,7 @@ class TestDevelopmentConfig(TestCase):
         self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
         self.assertFalse(current_app is None)
         self.assertTrue(app.config['SQLALCHEMY_DATABASE_URI'] == 
-                        os.environ.get('DATABASE_TEST_URL')
+                        os.environ.get('DATABASE_URL')
         )
 
 class TestTestingConfig(TestCase):
